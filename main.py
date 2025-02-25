44fd643c9f65efc882e67a3317f992e92c1bde24
charris-msft <charris@microsoft.com>
2025-02-25 11:21:26 -0800

    Add a new page for viewing uploaded files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, send_from_directory, abort
 from werkzeug.utils import secure_filename
 import os
 
@@ -28,6 +28,25 @@ def uploader():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return render_template('success.html', filename=filename)
+
+@app.route('/view')
+def view_files():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('view.html', files=files)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/view_text/<filename>')
+def view_text_file(filename):
+    try:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+        return render_template('view_text.html', filename=filename, content=content)
+    except:
+        abort(404)
 
 if __name__ == '__main__':
     print("Running manage.py")
